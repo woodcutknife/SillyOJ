31de6d79244be930dbc38805080f78f7b308d8d1
woodcutknife <woodcutknife@163.com>
2014-02-10 17:32:45 +0800

    modify oi-mode contest view and controller

--- app/controllers/hackers_controller.rb
+++ app/controllers/hackers_controller.rb
@@ -6,10 +6,11 @@ class HackersController < ApplicationController
     @contest.hackers.each { |x| sorted << x }
     @ac_cnt = {}
     mode = @contest.mode
-    @score = {} if mode == "oi"
+    @score = {} if mode == "OI"
     sorted.each do |hacker|
       rec = {}
-      hacker.solutions.each do |x|
+      solutions = hacker.solutions.where(contest_id: @contest.id)
+      solutions.each do |x|
         if rec.has_key? x.puzzle_id
           if x.id > rec[x.puzzle_id].id
             rec[x.puzzle_id] = x
@@ -18,23 +19,24 @@ class HackersController < ApplicationController
           rec[x.puzzle_id] = x
         end
       end
-      @score[hacker.id] = 0 if mode == "oi"
-      rec.each { |key, val| @score[hacker.id] += val.result.to_i } if mode == "oi"
-      @ac_cnt[hacker.id] = hacker.solutions.select { |x| x.result == (mode == "oj" ? "Accepted" : "100") and x.contest_id == @contest.id }.map { |y| y.puzzle_id }.uniq.size
+      @score[hacker.id] = 0 if mode == "OI"
+      rec.each { |key, val| @score[hacker.id] += val.result.to_f } if mode == "OI"
+      @ac_cnt[hacker.id] = solutions.select { |x| x.result == (mode == "OI" ? "100.0" : "Accepted") }.map { |y| y.puzzle_id }.uniq.size
     end
-    sorted.sort_by! { |hacker| [
+    if mode == "OI"
+      sorted.sort_by! { |hacker| [
+        -@score[hacker.id],
         -@ac_cnt[hacker.id],
         hacker.solutions.select { |x| x.contest_id == @contest.id }.size,
         hacker.id
-    ]}
-    p sorted
+      ]}
+    elsif mode == "ACM"
+      sorted.sort_by! { |hacker| [
+          -@ac_cnt[hacker.id],
+          hacker.solutions.select { |x| x.contest_id == @contest.id }.size,
+          hacker.id
+      ]}
+    end
     @hackers = Kaminari.paginate_array(sorted).page(params[:page]).per(50)
-  end
-
-  def show
-    @contest = Contest.find(params[:id])
-    check_contest_started!
-    @hacker = Hacker.find(params[:hid])
-    @solved = @hacker.solutions.select { |x| x.result == "Accepted" and x.contest_id == @contest.id }.map { |y| y.puzzle_id }.sort.uniq
   end
 end
